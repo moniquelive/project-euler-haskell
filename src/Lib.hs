5b380634b6bdfcbe79d377426e9724f0eab8ffb6
Cyber Oliveira <lccro@msn.com>
2020-07-27 18:20:53 -0300

    p14

--- src/Lib.hs
+++ src/Lib.hs
@@ -138,5 +138,18 @@ p11 = maximum [byLines, byColumns, byDiagonals]
 p16 :: Int
 p16 = sum $ map digitToInt $ show (2 ^ 1000)
 
-pCurrent = p16
+-- 837799
+p14 :: Int
+p14 = snd $ maximum $ zip (map (collatz 0) [1 .. 1000000]) [1 .. 1000000]
+ where
+  collatz :: Int -> Int -> Int
+  collatz _ 0 = error "Impossibru"
+  collatz l 1 = 1 + l
+  collatz l n = collatz (l + 1) (nxt n)
+   where
+    nxt :: Int -> Int
+    nxt n | even n    = div n 2
+          | otherwise = 3 * n + 1
+
+pCurrent = p14
 
